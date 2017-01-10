a78df814fdabd40cd9ee81a8f48e7aeac4ac72ac
Philip Dimitri Lourandos <philip.lourandos@absa.co.za>
2017-01-10 14:04:34 +0200

    DEV
    # Changed to Discovery client and not use eureka

--- service-shipping/src/main/java/com/absa/training/olsen/Application.java
+++ service-shipping/src/main/java/com/absa/training/olsen/Application.java
@@ -2,14 +2,16 @@ package com.absa.training.olsen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
  *
- * @author abpl245
+ * @author abpl245!
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
