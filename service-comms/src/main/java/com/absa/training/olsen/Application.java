f8c37e71b95ce43f735003d6382333b1f3c970d2
Philip Dimitri Lourandos <philip.lourandos@absa.co.za>
2017-01-10 12:33:52 +0200

    DEV
    # Default Application for service-comms project

---
+++ service-comms/src/main/java/com/absa/training/olsen/Application.java
@@ -0,0 +1,15 @@
+package com.absa.training.olsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author abpl245
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
